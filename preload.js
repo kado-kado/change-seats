f6498f44692b420c74a0d09ab905d5983003a335
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-05-14 00:41:54 +0900

    移植作業

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    closeApp: () => ipcRenderer.send('app-close'),
-    exportExcel: () => ipcRenderer.invoke('export-excel', seats2D)
-});
--- scripts/toggleUI.js
+++
@@ -1,6 +0,0 @@
-function toggleUI() {
-    const Neumorphism = document.getElementById('neumorphism');
-    if (!Neumorphism) return;
-
-    Neumorphism.disabled = !Neumorphism.disabled;
-}
